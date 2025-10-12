28b8704e33d9e4da6d4e8d6a75c656abfb891516
Leonard Yam <yamthesmall@gmail.com>
2025-10-12 21:46:16 +0800

    feat: use OpenZeppelin ReentrancyGuard

--- contracts/Campaign.sol
+++ contracts/Campaign.sol
@@ -1,32 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-// for safety 
-abstract contract ReentrancyGuardLite {
-    uint256 private _status;
-    constructor() { _status = 1; }
-    modifier nonReentrant() {
-        require(_status == 1, "Reentrancy");
-        _status = 2;
-        _;
-        _status = 1;
-    }
-}
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 
-contract Campaign is ReentrancyGuardLite {
-    struct Milestone{
-        string name;
-        string description;
-        milestoneStatus status;
-    }
-
-    enum milestoneStatus { Pending, Approved, Released, Rejected }
-    enum campaignStatus {Ongoing, Terminated, Completed}
-
-    event DonationReceived(address donor, uint256 amount);
-    event MilestoneApproved(uint256 index, string name, string description); //TODO1
-    event Refunded(uint256 amount); //TODO2
-
+contract Campaign is ReentrancyGuard {
     address owner;
     uint256 deadline; //TODO3
     string name;
@@ -35,9 +12,15 @@ contract Campaign is ReentrancyGuardLite {
     uint256 goal; // in ether, easier to manage
     uint256 totalRaised = 0;
     campaignStatus status;
+
     // mapping(address=>uint256) contributed (later do with refund code) TODO2
 
-    constructor (address _owner, uint256 _deadline, string memory _name, uint256 _goal) {
+    constructor(
+        address _owner,
+        uint256 _deadline,
+        string memory _name,
+        uint256 _goal
+    ) {
         owner = _owner;
         name = _name;
         deadline = _deadline;
@@ -47,53 +30,54 @@ contract Campaign is ReentrancyGuardLite {
     }
 
     modifier isOngoing() {
-        if (status == campaignStatus.Ongoing && block.timestamp >= deadline){
+        if (status == campaignStatus.Ongoing && block.timestamp >= deadline) {
             status = campaignStatus.Terminated;
         }
-        require(status == campaignStatus.Ongoing, "Campaign is not ongoing anymore!");
+        require(
+            status == campaignStatus.Ongoing,
+            "Campaign is not ongoing anymore!"
+        );
         _;
     }
 
     //TODO5 fallback function
-    function donate() public payable isOngoing(){
+    function donate() public payable isOngoing {
         uint256 donationAmount = msg.value / 1 ether;
-        require(donationAmount >= 1, "Minimum donation is 1 ether, donations are in the base value of 1 ether");
+        require(
+            donationAmount >= 1,
+            "Minimum donation is 1 ether, donations are in the base value of 1 ether"
+        );
         uint256 excess = msg.value % 1 ether;
         payable(msg.sender).transfer(excess);
         totalRaised += donationAmount;
         emit DonationReceived(msg.sender, donationAmount);
-        if (totalRaised >= goal){
+        if (totalRaised >= goal) {
             status = campaignStatus.Completed;
             //EMIT CAMPAIGN COMPLETED;
         }
     }
 
-    function getOwner() public view returns(address){
+    function getOwner() public view returns (address) {
         return owner;
     }
 
-    function getDeadline() public view returns(uint256){
+    function getDeadline() public view returns (uint256) {
         return deadline;
     }
 
-    function getGoal() public view returns(uint256){
+    function getGoal() public view returns (uint256) {
         return goal;
     }
 
-    function getTotalRaised() public view returns(uint256){
+    function getTotalRaised() public view returns (uint256) {
         return totalRaised;
     }
 
-    function getStatus() public view returns(campaignStatus){
+    function getStatus() public view returns (campaignStatus) {
         return status;
     }
 
-    function getName() public view returns(string memory){
+    function getName() public view returns (string memory) {
         return name;
     }
-
-    // deadline functions TODO3
-    //milestone functions TODO1
-    //let owner do be able to do all the setting up milestones while totalRaise is 0 for now
-    //we can add more options in the modifiers after
 }
