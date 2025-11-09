2941f85cafdc593aaa6ecadd9ff7c3075ebb21c2
Shanicey98 <shanice.chin86@gmail.com>
2025-11-09 17:03:07 +0800

    Remove my campaigns from all campaigns page
    - All campaigns page now only shows campaigns created by others and not by me

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,7 +3,9 @@ require("@nomicfoundation/hardhat-toolbox");
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.28",
-  url: "http://localhost:8545",
+  localhost: {
+    url: "http://localhost:8545",
+  },
  "cat.chuu.cc": {
       url: "http://cat.chuu.cc:8545",  // or "http://cat.chuu.cc:8545" if it’s plain HTTP
   },
